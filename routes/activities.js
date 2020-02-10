e9ba8faff83b27351af98d7bc2deb33f4214d5c9
Perdikal <Alexis.perd@yahoo.com>
2020-02-10 18:23:11 +0100

    started the activities route

--- routes/activities.js
+++ routes/activities.js
@@ -1,8 +1,11 @@
-const express = require("express");
-const router = require("router");
+/* const express = require("express");
+const router = express.Router();
 const Activity = require("../models/Activity");
+const router = require("router");
 const User = require("../models/User");
 
-router.get("/activities", (req, res, next) => {
-  res.render("activities");
+router.get("/activities/:activity", (req, res, next) => {
+  console.log(request.params);
 });
+
+module.exports = router; */
