f2b0fd9c0a45890a1f60a748ea034d07aa96fd1b
Perdikal <Alexis.perd@yahoo.com>
2020-02-10 14:44:02 +0100

    User schema updated

--- models/User.js
+++ models/User.js
@@ -1,15 +1,25 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  username: String,
-  password: String
-}, {
-  timestamps: {
-    createdAt: 'created_at',
-    updatedAt: 'updated_at'
+const userSchema = new Schema(
+  {
+    username: String,
+    password: String,
+    firstNAme: String,
+    lastName: String,
+    phoneNumber: String,
+    role: {
+      type: String,
+      enum: ["Admin", "regular member", "unregistered"]
+    }
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at"
+    }
   }
-});
+);
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 module.exports = User;
