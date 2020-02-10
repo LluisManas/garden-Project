386b25a8210c743826d91508c62f0aca1d42aca4
Lluís Mañas <manastorra@gmail.com>
2020-02-10 14:44:01 +0100

    Activity Schema

---
+++ models/Activity.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const activitySchema = new Schema(
+  {
+    activityName: String,
+    group: {
+      type: String,
+      enum: [
+        "General",
+        "Water",
+        "Compost",
+        "Mantainece",
+        "Trees and Bushes",
+        "Cleaning"
+      ]
+    },
+    description: String,
+    memebers: String,
+    creationDate: Date,
+    expirationDate: Date,
+    author: String
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at"
+    }
+  }
+);
+
+const Activity = mongoose.model("Activity", activityUser);
+module.exports = Activity;
