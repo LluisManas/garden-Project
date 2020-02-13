b15f1f6761d5ab2e1a114e203982b254dfa2b6f7
Lluís Mañas <manastorra@gmail.com>
2020-02-13 10:44:10 +0100

    mongoose connection

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ const MongoStore = require("connect-mongo")(session);
 const flash = require("connect-flash");
 
 mongoose
-  .connect("mongodb://localhost/garden-project", { useNewUrlParser: true })
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/movie-project", {
+    useNewUrlParser: true
+  })
   .then(x => {
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
